33f62b3f832b0fea0a83403e80a7dbc781991c4a
funa <funa@192.168.15.200>
2016-09-15 01:11:01 +0900

    2016/9/14 Twitter riha

--- src/TweenMain.java
+++ src/TweenMain.java
@@ -1,9 +1,17 @@
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.User;
 
 public class TweenMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws TwitterException {
 		// TODO 自動生成されたメソッド・スタブ
-
+		Twitter twi = new TwitterFactory().getInstance();
+		User user = twi.verifyCredentials();
+		
+		Status stat = twi.updateStatus("リハビリしています。");
 	}
 
 }
